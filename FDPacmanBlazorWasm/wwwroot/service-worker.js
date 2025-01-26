8be56e0ae26f25eb12c5fdefe64bcf9edd18ac16
filipdadgar <filip@dadgar.se>
2025-01-26 16:29:33 +0100

    fixed service worker

--- FDPacmanBlazorWasm/wwwroot/service-worker.js
+++ FDPacmanBlazorWasm/wwwroot/service-worker.js
@@ -1,4 +1,16 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+
+// Remove the no-op fetch event handler
+// self.addEventListener('fetch', () => { });
+
+self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+    // Perform install steps
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
+    // Perform activate steps
+});
